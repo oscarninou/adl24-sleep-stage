e1d6a8d3077d7588c3e2f25dc1b61e6821a5c962
Constant Bourdrez <constantbourdrez59@gmail.com>
2024-03-19 10:06:20 +0100

    made fft transform more understandable

--- compute_fft.py
+++ compute_fft.py
@@ -8,21 +8,20 @@ import numpy as np
 import torch as th
 import matplotlib.pyplot as plt
 from datetime import datetime
-from mne.io import read_raw_edf
+
 
 filepath = '/Users/constouille/Documents/GitHub/DL_ESPCI/adl24-sleep-stage/cassette-th-data.pck'
-data = np.load(filepath, allow_pickle = True)
-length = len(data[0][0])
-n_dim = len(data)
-n_subject = len(data[0])
+xtrain, xvalid, ytrain, yvalid = np.load(filepath, allow_pickle = True)
+length = len(xtrain[0])
+n_dim = 4
+n_subject = len(xtrain)
 n_subsample = 20
 size_subsample = length // n_subsample
 sliding_fft_tensor = th.ones(n_dim, n_subject, length)
 stop = False
 
-for i in range(n_dim//2):
-    signals = data[i]
-    print('yaaa')
+for signals in [xtrain, xvalid]:
+    print('Training' if (signals[0] == xtrain[0]).sum() == length else 'Validation')
     for j, signal in enumerate(signals):
         sliding_fft = []
         for balecouilles in range(n_subsample):
@@ -32,6 +31,5 @@ for i in range(n_dim//2):
         sliding_fft = np.array(sliding_fft)
         sliding_fft = sliding_fft.flatten()
         sliding_fft_tensor[i,j, :] = th.tensor(sliding_fft)
-        a = 2
         if stop:
             break
