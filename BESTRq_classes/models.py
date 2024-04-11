46604b6fc54b2cf31268850b05146c56571e0404
Constant Bourdrez <constantbourdrez59@gmail.com>
2024-04-11 13:42:40 +0200

    trained real bestrq

--- BESTRq_classes/models.py
+++ BESTRq_classes/models.py
@@ -211,3 +211,35 @@ class CNN(nn.Module):
         x = self.dropout(th.relu(self.fc1(x)))
         x = self.fc2(x)
         return x
+
+
+
+
+class CNN_multiproj(nn.Module):
+    def __init__(self, codebook_size=100, quantizer_dim =  100):
+        super(CNN_multiproj, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.dropout = nn.Dropout(p=0.5)
+        self.fc1 = nn.Linear(128 * 2, 512)
+        self.fc2 = nn.Linear(512, quantizer_dim)
+        self.fc3 = nn.Linear(9, 500)
+        self.fc4 = nn.Linear(500, codebook_size)
+        self.quantizer_dim = quantizer_dim
+        self.codebook_size = codebook_size
+    def forward(self, x):
+        x = self.pool(th.relu(self.conv1(x)))
+        x = self.pool(th.relu(self.conv2(x)))
+        x = self.pool(th.relu(self.conv3(x)))
+        x = self.pool(th.relu(self.conv4(x)))
+        x = x.view(-1, 128*2)
+        x = self.dropout(th.relu(self.fc1(x)))
+        x = self.fc2(x)
+        x = x.view(-1, 9)
+        x = self.dropout(th.relu(self.fc3(x)))
+        x = self.fc4(x)
+        x = x.view(-1, self.quantizer_dim, self.codebook_size)
+        return x
--- BESTRq_classes/train.py
+++ BESTRq_classes/train.py
@@ -241,3 +241,133 @@ def train_decoder(training_data, valid_data, decoder,encoder, epochs=10, lr=1e-3
         print(f"Epoch {epoch+1}/{epochs}, Train Loss: {avg_train_loss}, Validation Loss: {avg_valid_loss}, Training Accuracy: {train_accuracy}, Validation Accuracy: {valid_accuracy}")
 
     return train_losses, valid_losses, train_accuracies, valid_accuracies
+
+
+
+def pretrain_multiproj(training_data, valid_data, model , BestRQ, ratio_dataset = 2, epochs=10, lr=1e-3, device = 'cpu', raw_signal = True, batch_size = 200, device_for_proj = True):
+
+    xtrain, ytrain= training_data
+    xvalid, yvalid = valid_data
+
+    if device_for_proj == True:
+
+        # Convert tensors to device
+        xtrain_device = xtrain.to(device)
+        xvalid_device = xvalid.to(device)
+
+    else:
+        xtrain_device = xtrain
+        xvalid_device = xvalid
+
+
+    # Initialize empty lists to store the results
+    ytrain_batches = []
+    yvalid_batches = []
+
+    # Process training data in batches
+    for i in range(0, len(xtrain_device), batch_size):
+        x_batch = xtrain_device[i:min(i+batch_size, len(xtrain_device))]
+        y_batch = BestRQ(x_batch,)
+        ytrain_batches.append(y_batch)
+
+    # Process validation data in batches
+    for i in range(0, len(xvalid_device), batch_size):
+        x_batch = xvalid_device[i:min(i+batch_size, len(xvalid_device))]
+        y_batch = BestRQ(x_batch)
+        yvalid_batches.append(y_batch)
+
+    # Concatenate the batches
+    ytrain = th.cat(ytrain_batches, dim=0)
+    yvalid = th.cat(yvalid_batches, dim=0)
+    print('Projection done')
+
+    dataset_t = TensorDataset(xtrain, ytrain)
+    dataset_size = len(dataset_t)
+    half_dataset_size = dataset_size // ratio_dataset
+    dataset_t1, _ = random_split(dataset_t,  [half_dataset_size, dataset_size - half_dataset_size])
+    train_loader = DataLoader(dataset_t1, batch_size= batch_size, shuffle=True)
+    print('Training loader ok')
+
+    dataset_v = TensorDataset(xvalid, yvalid)
+    dataset_size = len(dataset_v)
+    half_dataset_size = dataset_size // ratio_dataset
+    dataset_v1, dataset_v2 = random_split(dataset_v,  [half_dataset_size, dataset_size - half_dataset_size])
+    valid_loader = DataLoader(dataset_v1, batch_size= batch_size, shuffle=True)
+    print('Validation loader ok')
+
+
+
+
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    loss_function = nn.CrossEntropyLoss()
+    softmax = nn.Softmax(dim = 2)
+
+    train_losses = []  # Pour sauvegarder la loss à chaque époch
+    valid_losses = []  # Pour sauvegarder la loss de validation à chaque époch
+    valid_wers = []    # Pour sauvegarder l'accuracy de validation à chaque époch
+    train_wers = []    # Pour sauvegarder l'accuracy de validation à chaque époch
+
+
+    print('Training started')
+    # Iterate over epochs
+    for epoch in tqdm(range(epochs)):
+        epoch_train_loss = 0.0
+        wrong = 0
+        total = 0
+
+        # Training phase
+        model.train()
+        for inputs, labels in train_loader:
+            optimizer.zero_grad()
+            inputs, labels = inputs.to(device), labels.to(device)
+            inputs, _ = mask(inputs, mask_prob = BestRQ.mask_prob, mask_time = BestRQ.mask_time, number_of_mask = BestRQ.num_masks_per_signal, device = inputs.device, raw_signal = raw_signal)
+            encoder_outs = model(inputs)
+            #encoder_outs = softmax(encoder_outs)
+            loss = loss_function(encoder_outs, labels)
+            loss.backward()
+            optimizer.step()
+            # Compute accuracy
+            predicted = th.argmax(encoder_outs, dim=1)
+            total += labels.size(0)
+            wrong += (predicted != labels.view_as(predicted)).sum().item() / labels.size(1)
+            epoch_train_loss += loss.item()
+
+        # Calculate average training loss for the epoch
+        avg_train_loss = epoch_train_loss / len(train_loader)
+        train_losses.append(avg_train_loss)
+        # Calculate validation accuracy
+        train_wer= wrong / total
+        train_wers.append(train_wer)
+
+        print('Validation')
+
+        # Validation phase
+        model.eval()
+        with th.no_grad():
+            epoch_valid_loss = 0.0
+            wrong = 0
+            total = 0
+            for inputs, labels in valid_loader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                encoder_outs = model(inputs)
+                #encoder_outs = softmax(encoder_outs)
+                loss = loss_function(encoder_outs, labels)
+                epoch_valid_loss += loss.item()
+
+                # Compute accuracy
+                predicted = th.argmax(encoder_outs, dim=1)
+                total += labels.size(0)
+                wrong += (predicted != labels.view_as(predicted)).sum().item() / labels.size(1)
+
+            # Calculate average validation loss for the epoch
+            avg_valid_loss = epoch_valid_loss / len(valid_loader)
+            valid_losses.append(avg_valid_loss)
+
+            # Calculate validation accuracy
+            valid_wer = wrong / total
+            valid_wers.append(valid_wer)
+
+        # Print epoch statistics
+        print(f"Epoch {epoch+1}/{epochs}, Train Loss: {avg_train_loss}, Validation Loss: {avg_valid_loss}, Training WER: {train_wer}, Validation WER: {valid_wer}")
+
+    return train_losses, valid_losses, train_wers, valid_wers
